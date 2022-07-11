16768928bf8a54f9db612457ca6c59e0dbda6615
sayTG <busarithienkie@gmail.com>
2022-07-11 22:32:02 +0100

    feat: adding connection string

--- OnboardingAPI/Program.cs
+++ OnboardingAPI/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using OnboardingAPI.Models.AppDbContext;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApiDbConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
